9de54bfd651455615650ed684821f1782728b13b
Arno Boenders <arno.boen@student.arteveldehs.be>
2024-07-25 22:25:17 +0200

    ✨Added routing to application

---
+++ src/pages/index.jsx
@@ -0,0 +1,10 @@
+import Home from "./Home";
+import Pokemon from "./Pokemon";
+import DetailPokemon from "./DetailPokemon";
+import Items from "./Items";
+import Moves from "./Moves";
+import Berries from "./Berries";
+// detail pages for berries, items, and moves for later
+import NotFound from "./NotFound";
+
+export { Home, Pokemon, DetailPokemon, Items, Moves, Berries, NotFound };
---
+++ src/routes/routes.jsx
@@ -0,0 +1,30 @@
+export const ROUTES = {
+    home: {
+        path: '/',
+        title: 'Home',
+    },
+    pokemon: {
+        path: '/pokemon',
+        title: 'Pokemon',
+    },
+    detailPokemon: {
+        path: '/pokemon/:slug',
+        title: 'Detail Pokemon',
+    },
+    items: {
+        path: '/items',
+        title: 'Items',
+    },
+    moves: {
+        path: '/moves',
+        title: 'Moves',
+    },
+    berries: {
+        path: '/berries',
+        title: 'Berries',
+    },
+    notFound: {
+        path: '/404',
+        title: 'Not Found',
+    },
+}
